3e326dca761e00e5ed14b622790dda8818a45fc1
Ehsan Titish <Maders@users.noreply.github.com>
2023-04-04 02:02:39 +0330

    feat: add mongodb persistence pipeline

--- fragmenty/core/pipelines.py
+++ fragmenty/core/pipelines.py
@@ -5,9 +5,42 @@
 
 
 # useful for handling different item types with a single interface
+import pymongo
 from itemadapter import ItemAdapter
 
 
 class CorePipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipeline:
+
+    collection_name = "numbers"  # Replace with your desired collection name
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].update_one(
+            # Filter - match the document with the same 'id'
+            {"id": item["id"]},
+            {"$set": item},  # Update the document with the new item data
+            upsert=True  # Insert a new document if no matching document is found
+        )
         return item
